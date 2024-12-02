b5e45a18afbf05119265ad7ac5d97c0a786a4a89
Trishan Preet Singh <99328557+Tripi-Wick@users.noreply.github.com>
2024-12-02 06:22:29 +0530

    Finally Done

--- init/data.js
+++ init/data.js
@@ -10,11 +10,11 @@ const sampleListings = [
     price: 1500,
     location: "Malibu",
     country: "United States",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [34.0259, -118.7798], // Latitude, Longitude for Malibu
+      coordinates: [34.0259, -118.7798],
     },
   },
   {
@@ -28,11 +28,11 @@ const sampleListings = [
     price: 1200,
     location: "New York City",
     country: "United States",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [40.7831, -73.9712], // Latitude, Longitude for New York City
+      coordinates: [40.7831, -73.9712],
     },
   },
   {
@@ -41,16 +41,16 @@ const sampleListings = [
       "Unplug and unwind in this peaceful mountain cabin. Surrounded by nature, it's a perfect place to recharge.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1519803505059-48b6b6e9a0c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1571896349842-33c89424de2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
     },
     price: 800,
     location: "Aspen",
     country: "United States",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [39.1911, -106.8175], // Latitude, Longitude for Aspen
+      coordinates: [39.1911, -106.8175],
     },
   },
   {
@@ -59,16 +59,16 @@ const sampleListings = [
       "This luxurious villa offers ample space, a private pool, and a tropical garden. A great choice for a family getaway.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1557152439-5518a7fe7be9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1566073771259-6a8506099945?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90ZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
     },
     price: 3500,
     location: "Bali",
     country: "Indonesia",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [-8.3405, 115.0920], // Latitude, Longitude for Bali
+      coordinates: [-8.3405, 115.0920],
     },
   },
   {
@@ -77,16 +77,16 @@ const sampleListings = [
       "Experience the charm of a rustic farmhouse surrounded by nature. Perfect for those looking for a peaceful escape.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1557152440-d2439b6c2c1c?ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1520250497591-112f2f40a3f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     },
     price: 900,
     location: "Upstate New York",
     country: "United States",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [42.0654, -73.8914], // Latitude, Longitude for Upstate New York
+      coordinates: [42.0654, -73.8914],
     },
   },
   {
@@ -95,16 +95,16 @@ const sampleListings = [
       "Live in luxury in this high-end penthouse with breathtaking views of the city skyline.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1557123741-0d167f2d32fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1622396481328-9b1b78cdd9fd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c2t5JTIwdmFjYXRpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
     },
     price: 5000,
     location: "Dubai",
     country: "United Arab Emirates",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [25.276987, 55.296249], // Latitude, Longitude for Dubai
+      coordinates: [25.276987, 55.296249],
     },
   },
   {
@@ -113,16 +113,16 @@ const sampleListings = [
       "Get away from the hustle and bustle in this cozy cabin nestled in the woods. Ideal for a winter retreat.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1519657043701-b5ff2e8b3858?ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vdW50YWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
     },
     price: 700,
     location: "Vermont",
     country: "United States",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [44.5588, -72.5770], // Latitude, Longitude for Vermont
+      coordinates: [44.5588, -72.5770],
     },
   },
   {
@@ -131,16 +131,16 @@ const sampleListings = [
       "This beautiful beachside villa offers stunning ocean views, an infinity pool, and luxurious amenities.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1557152439-5518a7fe7be9?ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGhvdGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
     },
     price: 4200,
     location: "Maldives",
     country: "Maldives",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [3.2028, 73.2207], // Latitude, Longitude for Maldives
+      coordinates: [3.2028, 73.2207],
     },
   },
   {
@@ -149,16 +149,16 @@ const sampleListings = [
       "This stunning luxury mansion offers expansive rooms, a grand pool, and sophisticated design. A once-in-a-lifetime stay.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1512540732-b990bfa4fc00?ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1502784444187-359ac186c5bb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNreSUyMHZhY2F0aW9ufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
     },
     price: 10000,
     location: "Los Angeles",
     country: "United States",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [34.0522, -118.2437], // Latitude, Longitude for Los Angeles
+      coordinates: [34.0522, -118.2437],
     },
   },
   {
@@ -167,36 +167,269 @@ const sampleListings = [
       "Relax and enjoy a slow-paced lifestyle at this picturesque countryside cottage surrounded by beautiful landscapes.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1508300782482-d229c52de3d1?ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG1vdW50YWlufGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
     },
-    price: 950,
-    location: "Kent",
+    price: 750,
+    location: "English Countryside",
     country: "United Kingdom",
-    owner: "673ced9a433bf28e32a2607d'",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [51.1791, 0.2113], // Latitude, Longitude for Kent, UK
+      coordinates: [51.5074, -0.1278],
     },
   },
   {
-    title: "Urban Apartment with Skyline View",
+    title: "Urban Chic Apartment",
     description:
-      "This chic urban apartment is the perfect spot to enjoy city life with incredible skyline views.",
+      "A sleek and modern apartment in the city's most vibrant district. Perfect for a stylish stay with easy access to nightlife and dining.",
     image: {
       filename: "listingimage",
-      url: "https://images.unsplash.com/photo-1584424111087-c9319e10d859?ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      url: "https://plus.unsplash.com/premium_photo-1661915661139-5b6a4e4a6fcc?q=80&w=2534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
-    price: 2200,
-    location: "Toronto",
-    country: "Canada",
-    owner: "673ced9a433bf28e32a2607d'",
+    price: 1600,
+    location: "Paris",
+    country: "France",
+    owner: "674ce8d1f784cc341b8faa6c",
     reviews: [],
     geometry: {
       type: "Point",
-      coordinates: [43.6532, -79.3832], // Latitude, Longitude for Toronto
+      coordinates: [48.8566, 2.3522],
+    },
+  },
+  {
+    title: "Desert Oasis Villa",
+    description:
+      "A luxurious villa in the heart of the desert. With private pools and panoramic views, it’s the perfect place to escape the ordinary.",
+    image: {
+      filename: "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 4800,
+    location: "Dubai Desert",
+    country: "United Arab Emirates",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [24.4539, 54.3773],
+    },
+  },
+  {
+    title: "Secluded Island Resort",
+    description:
+      "A private island getaway where you can enjoy total peace, crystal clear waters, and unparalleled luxury. The ultimate escape.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1510798831971-661eb04b3739?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 12000,
+    location: "Tahiti",
+    country: "French Polynesia",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [-17.6509, -149.4260],
+    },
+  },
+  {
+    title: "Lakefront Log Cabin",
+    description:
+      "This picturesque log cabin is located right on the lake, perfect for boating, fishing, and enjoying beautiful sunsets.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 1300,
+    location: "Lake Tahoe",
+    country: "United States",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [38.9398, -119.9741],
+    },
+  },
+  {
+    title: "City Penthouse with Rooftop Garden",
+    description:
+      "A luxurious penthouse featuring a private rooftop garden with breathtaking views. Perfect for city lovers looking for an upscale experience.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1472224371017-08207f84aaae?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 7000,
+    location: "Los Angeles",
+    country: "United States",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [34.0522, -118.2437],
+    },
+  },
+  {
+    title: "Tropical Eco-Lodge",
+    description:
+      "An eco-friendly tropical lodge located in a lush rainforest. Ideal for nature lovers and those seeking sustainability during their travels.",
+    image: {
+      filename: "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?q=80&w=2628&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 2200,
+    location: "Costa Rica",
+    country: "Costa Rica",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [9.7489, -83.7534],
+    },
+  },
+  {
+    title: "Victorian Mansion",
+    description:
+      "This elegant Victorian mansion offers grand architecture, antique furniture, and timeless charm. Experience history in style.",
+    image: {
+      filename: "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1661876449499-26de7959878f?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 3500,
+    location: "San Francisco",
+    country: "United States",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [37.7749, -122.4194],
+    },
+  },
+  {
+    title: "Modern Chalet in the Alps",
+    description:
+      "A stunning modern chalet located in the Swiss Alps. Ideal for skiing, hiking, or simply enjoying the majestic mountain views.",
+    image: {
+      filename: "listingimage",
+      url: "https://plus.unsplash.com/premium_photo-1661883964999-c1bcb57a7357?q=80&w=2628&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 5600,
+    location: "Zermatt",
+    country: "Switzerland",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [46.0207, 7.7491],
+    },
+  },
+  {
+    title: "Exclusive Private Mansion",
+    description:
+      "Experience the luxury of a private mansion in the heart of the city with top-tier amenities, perfect for hosting events or simply relaxing in style.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1481018085669-2bc6e4f00eed?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 12000,
+    location: "London",
+    country: "United Kingdom",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [51.5074, -0.1278],
+    },
+  },
+  {
+    title: "Mountain View Retreat",
+    description:
+      "A luxurious retreat nestled in the mountains, offering sweeping views, hiking trails, and serene surroundings. Ideal for nature lovers and adventurers.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1469401258206-4e0cc14e9358?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 3500,
+    location: "Aspen",
+    country: "United States",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [39.1911, -106.8175],
+    },
+  },
+  {
+    title: "Eco-Friendly Treehouse",
+    description:
+      "This unique treehouse combines eco-luxury and sustainability. Surrounded by forest, it offers a perfect escape from urban life while minimizing its environmental footprint.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1501635238895-63f29cfc06b3?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 2200,
+    location: "Vancouver Island",
+    country: "Canada",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [48.4284, -123.3656],
+    },
+  },
+  {
+    title: "Historical Mansion in the Countryside",
+    description:
+      "An elegant historical mansion with classic charm, set in the peaceful countryside. A perfect place to relax or host private events with an air of timeless luxury.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1459535653751-d571815e906b?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 8500,
+    location: "Bordeaux",
+    country: "France",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [44.8404, -0.5805],
+    },
+  },
+  {
+    title: "Luxury Ski Lodge",
+    description:
+      "This luxury ski lodge offers ski-in/ski-out access, a cozy fireplace, and all the amenities needed for an unforgettable winter vacation in the Swiss Alps.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1570129477492-45c003edd2be?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 7200,
+    location: "St. Moritz",
+    country: "Switzerland",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [46.4984, 9.8380],
+    },
+  },
+  {
+    title: "Luxury Beachfront Villa",
+    description:
+      "A beachfront villa offering panoramic ocean views, private beach access, and spacious living areas. Perfect for an exclusive getaway.",
+    image: {
+      filename: "listingimage",
+      url: "https://images.unsplash.com/photo-1504233529578-6d46baba6d34?q=80&w=2674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    price: 15000,
+    location: "Maldives",
+    country: "Maldives",
+    owner: "674ce8d1f784cc341b8faa6c",
+    reviews: [],
+    geometry: {
+      type: "Point",
+      coordinates: [3.2028, 73.2207],
     },
   },
 ];
-
   module.exports = { data: sampleListings };
--- init/index.js
+++ init/index.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const dbUrl = "mongodb+srv://kimmu1370:TdhtQEDHulkpxlSQZ@cluster0.th74p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const dbUrl = "mongodb+srv://Plz-Chal-ja-cry:1234@cluster0.th74p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
 
 async function main() {
     await mongoose.connect(dbUrl);
+    // await mongoose.connect(MONGO_URL);
+
 }
 
 main()
@@ -18,8 +20,8 @@ main()
     });
 
 const initDB = async() =>{
-    // await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj,owner:"673ced9a433bf28e32a2607d"}))
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj,owner:"674ce8d1f784cc341b8faa6c"}))
     await Listing.insertMany(initData.data)
     console.log("Data was initialized");
 }
